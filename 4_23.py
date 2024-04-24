6875409e65dc9a4e13231e51d2c03448a5fb20ae
Omar Nassar <omariio1000@gmail.com>
2024-04-23 20:52:24 -0700

    quiz 4/23

---
+++ 4_23.py
@@ -0,0 +1,30 @@
+from scipy import signal
+from math import pi
+import matplotlib.pyplot as plt
+import os
+
+
+R = 1
+L = 2.25e-5
+C = 4.5e-5
+
+num = [0, 0, 1/(L * C)]
+dem = [1, R/L, 1/(L * C)]
+
+tfun = signal.TransferFunction(num, dem)
+w, mag, phase = signal.bode(tfun)
+f = w / (2 * pi)
+
+fig, ax = plt.subplots(nrows=2)
+ax[0].semilogx(f, mag)
+ax[0].set_ylabel('Magnitude (dB)')
+ax[0].set_title('Bode Plot\n')
+ax[0].grid()
+ax[1].semilogx(f, phase)
+ax[1].set_ylabel('Phase (degrees)')
+ax[1].set_xlabel('Frequency (Hz)')
+ax[1].grid()
+
+file_name = f'{os.path.dirname(__file__)}\\quiz_4_9_bode.png'
+plt.savefig(file_name)
+plt.close()
