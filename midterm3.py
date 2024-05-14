d88258a0e4a25aaa2db790a9f01ce81c99c3651f
Omar Nassar <omariio1000@gmail.com>
2024-05-14 14:10:35 -0700

    add midterm stuff

---
+++ midterm3.py
@@ -0,0 +1,23 @@
+from scipy import signal
+from math import pi
+import matplotlib.pyplot as plt
+
+
+num = [1, 0]
+dem = [-1, -4, -1]
+
+tfun = signal.TransferFunction(num, dem)
+w, mag, phase = signal.bode(tfun)
+f = w / (2 * pi)
+
+fig, ax = plt.subplots(nrows=2)
+ax[0].semilogx(f, mag)
+ax[0].set_ylabel('Magnitude (dB)')
+ax[0].set_title('Bode Plot\n')
+ax[0].grid()
+ax[1].semilogx(f, phase)
+ax[1].set_ylabel('Phase (degrees)')
+ax[1].set_xlabel('Frequency (Hz)')
+ax[1].grid()
+
+plt.show()
